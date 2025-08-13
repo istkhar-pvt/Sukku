44e84c632c7e4a64439a060d06a6816dd7154f06
AllSystem88 <deependray228@gmail.com>
2025-08-13 10:04:25 +0530

    sʏsᴛᴇᴍ

--- config.py
+++ config.py
@@ -60,7 +60,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DeepVikky88/SukkuMusic",
+    "https://github.com/AllSystem88/SukkuMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
