b553c5a6774e01c6c67458dd0793c4938c99ae40
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2025-12-03 21:21:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ ASSUSERNAME = getenv("ASSUSERNAME")
 # ---------------------------------------------------------
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", 'NxGBNexGenBots254c6b') # youtube song api ke # -------------------------------------------------------------------------# -------------------------------------------------------------------------#
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots4556e3') # youtube song api ke # -------------------------------------------------------------------------# -------------------------------------------------------------------------#
 
 
 # For - downloads
